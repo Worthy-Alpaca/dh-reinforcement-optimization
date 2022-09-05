167dca6aeb64697ba43f5f7dca7c78650dcdef30
Worthy-Alpaca <worthyalpaca@gmail.com>
2022-09-05 14:18:33 +0200

    added classes for custom datasets and dataloader

---
+++ src/misc/dataset.py
@@ -0,0 +1,31 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import math
+from scipy.spatial import distance_matrix
+
+
+class ProductDataset(Dataset):
+    def __init__(self, currentDict, clist) -> None:
+        super().__init__()
+        self.data = torch.from_numpy(currentDict[:, :3].astype(np.float32))
+        self.components = clist
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        # hier index ziehen
+        x = self.components[index]
+        return x, self.data[index]
+        
+
+
+class ProductDataloader(DataLoader):
+    def __iter__(self):
+         for batch in super().__iter__():
+            # print(batch)
+            # test = self.sampler.data_source.refData
+            yield batch[1], distance_matrix(batch[1], batch[1]), batch[0]
+
+    
