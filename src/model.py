1487042ff539c2e2f570b13cf93561b5a873ffa8
Worthy-Alpaca <worthyalpaca@gmail.com>
2022-09-05 14:16:45 +0200

    added device casting for all internal layers

--- src/model.py
+++ src/model.py
@@ -17,16 +17,16 @@ class QNetModel(nn.Module):
 
         nr_extra_layers_1 = T
 
-        self.theta1 = nn.Linear(self.node_dim, self.emb_dim, True)
-        self.theta2 = nn.Linear(self.emb_dim, self.emb_dim, True)
-        self.theta3 = nn.Linear(self.emb_dim, self.emb_dim, True)
-        self.theta4 = nn.Linear(1, self.emb_dim, True)
-        self.theta5 = nn.Linear(2 * self.emb_dim, 1, True)
-        self.theta6 = nn.Linear(self.emb_dim, self.emb_dim, True)
-        self.theta7 = nn.Linear(self.emb_dim, self.emb_dim, True)
+        self.theta1 = nn.Linear(self.node_dim, self.emb_dim, True, device=self.device)
+        self.theta2 = nn.Linear(self.emb_dim, self.emb_dim, True, device=self.device)
+        self.theta3 = nn.Linear(self.emb_dim, self.emb_dim, True, device=self.device)
+        self.theta4 = nn.Linear(1, self.emb_dim, True, device=self.device)
+        self.theta5 = nn.Linear(2 * self.emb_dim, 1, True, device=self.device)
+        self.theta6 = nn.Linear(self.emb_dim, self.emb_dim, True, device=self.device)
+        self.theta7 = nn.Linear(self.emb_dim, self.emb_dim, True, device=self.device)
 
         self.theta1_extras = [
-            nn.Linear(self.emb_dim, self.emb_dim, True)
+            nn.Linear(self.emb_dim, self.emb_dim, True, device=self.device)
             for _ in range(nr_extra_layers_1)
         ]
 
@@ -63,34 +63,3 @@ class QNetModel(nn.Module):
         if torch.any(torch.isnan(out)):
             print()
         return out2
-        # num_nodes = xv.shape[1]
-        # batch_size = xv.shape[0]
-
-        # conn_matrices = torch.where(
-        #     Ws > 0, torch.ones_like(Ws), torch.zeros_like(Ws)
-        # ).to(self.device)
-
-        # mu = torch.zeros(batch_size, num_nodes, self.emb_dim, device=self.device)
-        # s1 = self.theta1(xv)
-        # for layer in self.theta1_extras:
-        #     s1 = layer(F.relu(s1))
-
-        # s3_1 = F.relu(self.theta4(Ws.unsqueeze(3)))
-        # s3_2 = torch.sum(s3_1, dim=1)
-        # s3 = self.theta3(s3_2)
-
-        # for t in range(self.T):
-        #     s2 = self.theta2(conn_matrices.matmul(mu))
-        #     mu = F.relu(s1 + s2 + s3)
-
-        # global_state = self.theta6(
-        #     torch.sum(mu, dim=1, keepdim=True).repeat(1, num_nodes, 1)
-        # )
-
-        # local_action = self.theta7(mu)
-
-        # out = F.relu(torch.cat([global_state, local_action], dim=2))
-        # out2 = self.theta5(out).squeeze(dim=2)
-        # if torch.any(torch.isnan(out)):
-        #     print()
-        # return out2
