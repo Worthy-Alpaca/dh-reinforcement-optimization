28116ae660135c625e48ea3e8f7ebad678b642d1
Worthy-Alpaca <worthyalpaca@gmail.com>
2022-09-07 13:36:06 +0200

    added methods for solution display

--- src/modules/gui/controller/controller.py
+++ src/modules/gui/controller/controller.py
@@ -1,5 +1,9 @@
 import random
 import tkinter as tk
+import numpy as np
+from sqlalchemy import create_engine
+from logging import info
+from helper import Memory, UtilFunctions, Cartsetup, Coating
 from matplotlib.pyplot import style
 
 try:
@@ -16,12 +20,11 @@ class Controller(MyCanvas):
 
     def __call__(
         self,
-        coords: dict,
-        mTime: dict,
-        sTime: dict,
-        numParts: int,
-        randomInterupt: tuple = (0, 0),
-        prodName: str = "",
+        best_value,
+        best_solution,
+        dbpath: str,
+        calcGroups: bool = False,
+        overlapThreshhold: float = 0.5,
     ) -> None:
         """Creates the summary with the provided data.
 
@@ -33,37 +36,71 @@ class Controller(MyCanvas):
             randomInterupt (tuple, optional): The random interrupt values. (Min, Max). Defaults to (0, 0).
             prodName (str, optional): The Product name. Defaults to "".
         """
-        self.figure.clear()
-        plot = self.figure.add_subplot(121)
-        ax = self.figure.add_subplot(122)
-        runtime = sum(list(mTime.values()))
-        # setupTime = sum(list(sTime.values()))
-        ax.axis("off")
-        sumtime = []
-        for i in range(numParts):
-            sumtime.append(
-                runtime + random.randint(randomInterupt[0], randomInterupt[1])
-            )
 
-        textstr = "\n".join(
-            (
-                f"Product: {prodName} ",
-                "",
-                f"Overall time needed: {round(sum(sumtime), 2)} Seconds",
-                f"Average Time: {round(sum(sumtime) / numParts, 2)} Seconds ",
-                f"Highest time: {round(max(sumtime), 2)} Seconds",
-                "",
-                "Machines",
-            )
+        self.best_value = best_value
+        self.overlapThreshhold = overlapThreshhold
+        self.solution = best_solution["solution"]
+        self.coords = best_solution["coords"]
+        self.components = best_solution["components"]
+        self.numCarts = best_solution["numCarts"]
+        self.products = best_solution["products"]
+        if calcGroups:
+            self.validate = False
+        else:
+            self.validate = True
+        self.helper = UtilFunctions(self.components)
+        self.engine = create_engine(f"sqlite:///{dbpath}")
+
+        info(
+            f"The best value for this iteration is: {self.best_value}",
         )
-        substr = ""
-        for key in mTime:
-            substr = (
-                substr
-                + f"\n{key} Ideal: {round(mTime[key], 2)} Seconds \n{key} Setup Time: {round(sTime[key], 2)} Seconds\n"
-            )
-        textstr = textstr + substr
-        props = dict(boxstyle="round", alpha=0.5)
+
+        groupTimings = self.__plot_solution(
+            self.coords,
+            self.solution,
+            validate=self.validate,
+        )
+        self.canvas.draw()
+
+    def __plot_solution(self, coords: np.ndarray, solution: list, validate=False):
+        """Method to plot the given coordinates according to the give solution.
+
+        Args:
+            coords (np.ndarray): The current coordinate set.
+            solution (list): The calculated solution.
+        """
+
+        self.figure.clear()
+
+        labels = coords[:, 3:4]
+        labels = labels[:, 0].tolist()
+
+        solutionList = []
+        for x in solution:
+            solutionList.append(coords[x][3:4][0])
+
+        if not validate:
+            solutionList = self.__calcGroups(solutionList)
+        SETUPMINUTES = 10
+        # groupTimings = len(solutionList) * SETUPMINUTES * 60
+        groupTimings = 0
+        textstr = f"{len(solutionList)} Groups\n"
+        testArr = []
+        for x in solutionList:
+            textstr += f"{x}\n"
+            runningArr = []
+            if not validate:
+                for i in x:
+                    runningArr.append(labels.index(i))
+                testArr.append(runningArr)
+
+        coords = coords[:, :3].astype(np.float32)
+        plot = self.figure.add_subplot(121, projection="3d")
+        ax = self.figure.add_subplot(122)
+        ax.axis("off")
+        plot.scatter(coords[:, 0], coords[:, 1], coords[:, 2])
+
+        n = len(coords)
 
         ax.text(
             0.05,
@@ -74,10 +111,97 @@ class Controller(MyCanvas):
             verticalalignment="top",
             wrap=True,
         )
-        for key in coords:
-            plot.scatter(coords[key]["X"], coords[key]["Y"])
-        plot.legend(tuple(coords.keys()), loc="upper left")
-        self.canvas.draw()
+        ax.set_title(
+            "model / overlap = %.5f s"
+            % (
+                self.helper.calc_total_time(
+                    self.solution,
+                )[0]
+                # + groupTimings
+            )
+        )
+
+        for idx in range(n - 1):
+            i, next_i = solution[idx], solution[idx + 1]
+            plot.plot(
+                [coords[i, 0], coords[next_i, 0]],
+                [coords[i, 1], coords[next_i, 1]],
+                [coords[i, 2], coords[next_i, 2]],
+                "k",
+                lw=2,
+                alpha=0.8,
+            )
+
+        i, next_i = solution[-1], solution[0]
+        plot.plot(
+            [coords[i, 0], coords[next_i, 0]],
+            [coords[i, 1], coords[next_i, 1]],
+            [coords[i, 2], coords[next_i, 2]],
+            "k",
+            lw=2,
+            alpha=0.8,
+        )
+        plot.set(
+            xlabel="Number of placements",
+            ylabel="Number of Components",
+            zlabel="Cumulative Component Score",
+        )
+        # plot.xlabel("Number of placements")
+        # plot.ylabel("Number of Components")
+        plot.plot(
+            coords[solution[0], 0],
+            coords[solution[0], 1],
+            coords[solution[0], 2],
+            "x",
+            markersize=10,
+        )
+        return groupTimings
+
+        # self.figure.clear()
+        # plot = self.figure.add_subplot(121)
+        # ax = self.figure.add_subplot(122)
+        # runtime = sum(list(mTime.values()))
+        # # setupTime = sum(list(sTime.values()))
+        # ax.axis("off")
+        # sumtime = []
+        # for i in range(numParts):
+        #     sumtime.append(
+        #         runtime + random.randint(randomInterupt[0], randomInterupt[1])
+        #     )
+
+        # textstr = "\n".join(
+        #     (
+        #         f"Product: {prodName} ",
+        #         "",
+        #         f"Overall time needed: {round(sum(sumtime), 2)} Seconds",
+        #         f"Average Time: {round(sum(sumtime) / numParts, 2)} Seconds ",
+        #         f"Highest time: {round(max(sumtime), 2)} Seconds",
+        #         "",
+        #         "Machines",
+        #     )
+        # )
+        # substr = ""
+        # for key in mTime:
+        #     substr = (
+        #         substr
+        #         + f"\n{key} Ideal: {round(mTime[key], 2)} Seconds \n{key} Setup Time: {round(sTime[key], 2)} Seconds\n"
+        #     )
+        # textstr = textstr + substr
+        # props = dict(boxstyle="round", alpha=0.5)
+
+        # ax.text(
+        #     0.05,
+        #     0.95,
+        #     textstr,
+        #     transform=ax.transAxes,
+        #     fontsize=14,
+        #     verticalalignment="top",
+        #     wrap=True,
+        # )
+        # for key in coords:
+        #     plot.scatter(coords[key]["X"], coords[key]["Y"])
+        # plot.legend(tuple(coords.keys()), loc="upper left")
+        # self.canvas.draw()
 
     def wait(self, message: str = "") -> any:
         """Function that displays a loading screen."""
@@ -122,3 +246,82 @@ class Controller(MyCanvas):
             wrap=True,
         )
         self.canvas.draw()
+
+    def __calcGroups(self, solutionList):
+        """Method to calculate batch queues.
+
+        Args:
+            solutionList (list): The current products according to predicted solution.
+
+        Returns:
+            list: A list containing the batch lists.
+        """
+        maxSlots = 36 * self.numCarts * 8
+        runningSlots = 0
+        solutionListRunning = []
+        solutionListReturn = []
+        for i in range(len(solutionList)):
+            product = solutionList[i]
+            Components = self.products[product]["comps"]
+            try:
+                ComponentsNext = self.products[solutionList[i + 1]]["comps"]
+            except:
+                ComponentsNext = []
+            overlapComponents = list(set(Components) & set(ComponentsNext))
+            x = len(overlapComponents) / len(Components)
+            if len(overlapComponents) / len(Components) < self.overlapThreshhold:
+                solutionListRunning.append(product)
+                solutionListReturn.append(solutionListRunning.copy())
+                solutionListRunning.clear()
+                continue
+            slotSize = self.__calcSlotSize(Components)
+            slotSizeOverlap = self.__calcSlotSize(overlapComponents)
+            numComponents = slotSize - slotSizeOverlap
+            if numComponents + runningSlots < maxSlots:
+                runningSlots += numComponents
+                solutionListRunning.append(product)
+            else:
+                # runningSlots = 0
+                solutionListReturn.append(solutionListRunning.copy())
+                solutionListRunning.clear()
+                solutionListRunning.append(product)
+                runningSlots = slotSize
+        solutionListReturn.append(solutionListRunning)
+        b = []
+        for lng in range(len(solutionListReturn)):
+            if len(solutionListReturn[lng]) >= 1:
+                b.append(solutionListReturn[lng])
+        solutionListReturn = b
+        return solutionListReturn
+
+    def __calcSlotSize(self, components) -> int:
+        """Method to calculate the slot size for the given component list.
+
+        Args:
+            components (list): The current component list.
+
+        Returns:
+            int: The cumulative size of the component feeders.
+        """
+        numComponents = 0
+        for i in components:
+            with self.engine.begin() as connection:
+                result = connection.execute(
+                    f"SELECT * FROM 'ReferenceComponents' WHERE Component = '{i}'"
+                ).first()
+                if result == None:
+                    result = {"Feedersize": 8}
+                else:
+                    result = result._asdict()
+                size = result["Feedersize"]
+            if i == "Kreis 1.5mm Bildver" or i == "ATOM":
+                numComponents += 0
+            elif size == "Barcode":
+                numComponents += 40
+            elif size == None:
+                numComponents += 8
+            elif size == "MSF16" or int(size) == 12:
+                numComponents += 16
+            else:
+                numComponents += int(size)
+        return numComponents
